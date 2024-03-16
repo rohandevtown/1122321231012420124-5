70b5d7e356a5bcaab27166945ffa0d56f70375e3
rohandevtown <rohankinnal@devtown.in>
2024-03-16 20:11:52 +0530

    d4

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {books} = require("../data/books.json");
 const {users} = require("../data/users.json");
+const { route } = require("./books");
 
 const router = express.Router();
 
@@ -43,7 +44,7 @@ router.get("/:id",(req, res)=>{
 
 
 /**
- * Route: /books/issued
+ * Route: /books/issued//by-user
  * Method: GET
  * Description: Get all issued books
  * Access: Public
@@ -78,4 +79,72 @@ router.get("/issued/by-user", (req, res)=>{
 });
 
 
+
+/**
+ * Route: /books
+ * Method: POST
+ * Description: Create a new Book
+ * Access: Public
+ * Paramaters: None
+ * Data: author, name, genre, price, publisher, id
+ */
+router.post('/',(req, res)=>{
+    const {data} = req.body;
+
+    if(!data)
+    return res.status(404).json({
+success: false,
+message: "No data provided"
+})
+
+    const book = books.find((each) => each.id === data.id)
+    if(book)
+        return res.status(404).json({
+    success: false,
+    message: "User with the given id already exists :-( "
+        })
+
+const allBooks = [...books, data];
+
+return res.status(201).json({
+    success: true,
+    data: allBooks
+})
+})
+
+
+/**
+ * Route: /books/:id
+ * Method: PUT
+ * Description: Updating a book by their id
+ * Access: Public
+ * Paramaters: Id
+ * Data: author, name, genre, price, publisher, id
+ */
+router.put("/:id", (req, res)=>{
+    const {id} = req.params;
+    const {data} = req.body;
+
+    const book = books.find((each)=>each.id ===id)
+    if(!book)
+        return res.status(404).json({
+            success: false,
+            message: "Book with the given id doesn't exist :-("
+        })
+
+        const updateData = books.map((each)=>{
+            if(each.id === id){
+                return {...each, ...data}
+            }
+            return each
+        });
+        return res.status(200).json({
+            success: true,
+            data: updateData
+        })
+})
+
+
 module.exports = router;
+
+
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {users} = require("../data/users.json");
+const { route } = require("./books");
 
 const router = express.Router();
 
@@ -130,5 +131,74 @@ users.splice(index, 1);
 
 return res.status(200).json({success: true, data: users})
 })
+
+
+
+
+
+/**
+ * Route: /users/subscription-details/:id
+ * Method: GET
+ * Description: Get all subscription details
+ * Access: Public
+ * Paramaters: Id
+ */
+router.get('/subscription-details/:id', (req, res)=>{
+    const {id} = req.params;
+
+    const user = users.find((each)=> each.id === id);
+
+    if(!user)
+        return res.status(404).json({
+            success: false,
+            message: "User Not Found :-("
+        })
+
+        const getDateInDays = (data = "") =>{
+            let date;
+            if(data === ""){
+                // current Date
+                date = new Date();
+            }else{
+                // getting data on basis of data variable
+                date = new Date(data)
+            }
+            let days = Math.floor(date / (1000 * 60 * 60 * 24));
+            return days
+        };
+
+        const subscriptionType = (date) => {
+            if(user.subscriptionType === "Basic"){
+                date = date + 90;
+            }else if(user.subscriptionType === "Standard"){
+                date = date + 180;
+            }else if(user.subscriptionType === "Premium"){
+                date = date + 365;
+            }
+            return date;
+        };
+
+        // Subscription Claculation Expiration Logic
+        // Jan 1, 1970, UTC // milli seconds
+        let returnDate = getDateInDays(user.returnDate);
+        let currentDate = getDateInDays();
+        let subscriptionDate = getDateInDays(user.subscriptionDate);
+        let subscriptionExpiration  = subscriptionType(subscriptionDate);
+
+        const data = {
+            ...user,
+            subscriptionExpired: subscriptionExpiration < currentDate,
+            daysLeftForExpiration: subscriptionExpiration <= currentDate ? 0 : subscriptionExpiration - currentDate,
+            fine: returnDate < currentDate ? subscriptionExpiration <= currentDate ? 200 : 100 : 0
+        }
+        return res.status(200).json({
+            success: true,
+            data
+        })
+})
+
+
+
+
 
 module.exports = router;
